129330e7ff24df55f569f47ca98a3c72d77cb2c7
Browf1st <102019023+Browf1st@users.noreply.github.com>
2023-01-15 15:45:17 +0800

    Pili ka na skin lods

--- My project/Assets/Codes/Customize.cs
+++ My project/Assets/Codes/Customize.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class Customize : MonoBehaviour
 
@@ -17,7 +18,7 @@ public class Customize : MonoBehaviour
     public SpriteRenderer Rleg;
     public SpriteRenderer Lleg;
 
-    public string Name;
+     public string Name;
 
 
    
@@ -30,22 +31,34 @@ public class Customize : MonoBehaviour
     public List<Sprite> Rarms = new List<Sprite>();
     public List<Sprite> Larms = new List<Sprite>();
     public List<Sprite> Rlegs = new List<Sprite>();
-    public List<Sprite> Llegs = new List<Sprite>();
-
+    public List<Sprite> Llegs = new List<Sprite>();  
+    public List<String> CharacterName = new List<String>();  
+  
     private static int currentOption = 0;
 
+
     private void Start()
-    {
-        Name = "Caloocan Boy";
+    {  
+       Name = "Caloocan Boy";
+        MySelectedCharacter(PlayerPrefs.GetInt("MySelectedCharacter_Int"));
         
+        Debug.Log("Num : "+currentOption);
     }
     public void NextOption()
     {
-        currentOption++;
         if (currentOption >= Heads.Count)
         {
             currentOption = 0;
         }
+        else
+        {
+            currentOption+=1;
+           
+            
+            PlayerPrefs.SetInt("MySelectedCharacter_Int", currentOption);
+            PlayerPrefs.Save();
+        }
+       
 
         Head.sprite = Heads[currentOption];
         Body.sprite = Bodys[currentOption];
@@ -53,15 +66,31 @@ public class Customize : MonoBehaviour
         Larm.sprite = Larms[currentOption];
         Rleg.sprite = Rlegs[currentOption];
         Lleg.sprite = Llegs[currentOption];
+       // Name = CharacterName[currentOption];
 
      
     }
 
 
-    public void submit()
-    {
+    
+    public void MySelectedCharacter(int thisOption)
+    {   
+        if(SceneManager.GetActiveScene().name != "Customize")
+        {
+            Head.sprite = Heads[thisOption];
+            Body.sprite = Bodys[thisOption];
+            Rarm.sprite = Rarms[thisOption];
+            Larm.sprite = Larms[thisOption];
+            Rleg.sprite = Rlegs[thisOption];
+            Lleg.sprite = Llegs[thisOption];
+            Name = CharacterName[thisOption];
+        }
+     
+    }
 
-        
+
+    public void submit()
+    { 
             SceneManager.LoadScene(1);
      
     }
